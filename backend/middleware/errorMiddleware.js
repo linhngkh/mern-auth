6efa1cde10d7d6d3884649381a1c522538585680
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-05-15 18:35:25 +0300

    created middleware which handled error

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,15 @@
+const notFound = (req, res, next) => {
+  const error = new Error(`Not found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (error, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = error.message;
+
+  if (error.name === "CastError" && error.kind === "ObjectId") {
+    statusCode = 404;
+    message = "Resource not found";
+  }
+};
